efbad65d60cf270188d224f7f0b385d95e265a33
Dave MacFarlane <driusan@gmail.com>
2016-01-05 20:46:09 -0500

    Initial implementation of bug commit for Hg

--- issues/Roadmap.md
+++ issues/Roadmap.md
@@ -1,12 +1,15 @@
 # Roadmap for bug
 
 ## v0.3:
+- More options for bug create
 - bug-serve should have feature parity with bug
 - bug-import tool should be able to import from other issue trackers
-- More options for bug roadmap
-- More options for bug create
 - Include method of defining stable identifiers
+- More options for bug roadmap
 
 ## v0.2:
 - bug list should have option to display tags
 - Add support for other source control engines
+
+## No milestone set:
+- adf
---
+++ scm/HgManager.go
@@ -0,0 +1,32 @@
+package scm
+
+import (
+	"fmt"
+	"github.com/driusan/bug/bugs"
+	"os/exec"
+)
+
+type HgManager struct{}
+
+func (a HgManager) Purge(dir bugs.Directory) error {
+	return UnsupportedType("Purge is not supported under Hg. Sorry!")
+}
+
+func (a HgManager) Commit(dir bugs.Directory, commitMsg string) error {
+	cmd := exec.Command("hg", "add", string(dir))
+	if err := cmd.Run(); err != nil {
+		fmt.Printf("Could not add issues to be commited: %s?\n", err.Error())
+		return err
+	}
+
+	cmd = exec.Command("hg", "commit", string(dir), "-m", commitMsg)
+	if err := cmd.Run(); err != nil {
+		fmt.Printf("No new issues to commit.\n")
+		return err
+	}
+	return nil
+}
+
+func (a HgManager) GetSCMType() string {
+	return "hg"
+}
