2bfa21082d50bcac8417bc97785d18b09f0b03f6
Dave MacFarlane <driusan@gmail.com>
2016-01-12 14:58:00 -0500

    Added bug-import --be to import BugsEverywhere database

--- bug-import/main.go
+++ bug-import/main.go
@@ -2,55 +2,9 @@ package main
 
 import (
 	"fmt"
-	"github.com/driusan/bug/bugs"
-	"github.com/google/go-github/github"
 	"os"
 	"strings"
 )
-
-func githubImport(user, repo string) {
-	client := github.NewClient(nil)
-	issueDir := bugs.GetIssuesDir()
-	opt := &github.IssueListByRepoOptions{
-		ListOptions: github.ListOptions{PerPage: 100},
-	}
-	issues, resp, err := client.Issues.ListByRepo(user, repo, opt)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		return
-	}
-
-	for lastPage := false; lastPage != true; {
-		for _, issue := range issues {
-			if issue.PullRequestLinks == nil {
-				b := bugs.Bug{issueDir + bugs.TitleToDir(*issue.Title)}
-				if dir := b.GetDirectory(); dir != "" {
-					os.Mkdir(string(dir), 0755)
-				}
-				if issue.Body != nil {
-					b.SetDescription(*issue.Body)
-				}
-				if issue.Milestone != nil {
-					b.SetMilestone(*issue.Milestone.Title)
-				}
-				// Don't set a bug identifier, but put an empty line and
-				// then a GitHub identifier, so that bug commit can include
-				// "Closes ..." in the commit message.
-				b.SetIdentifier(fmt.Sprintf("\n\nGitHub:%s/%s%s%d\n", user, repo, "#", *issue.Number))
-				for _, l := range issue.Labels {
-					b.TagBug(bugs.Tag(*l.Name))
-				}
-				fmt.Printf("Importing %s\n", *issue.Title)
-			}
-		}
-		if resp.NextPage == 0 {
-			lastPage = true
-		} else {
-			opt.ListOptions.Page = resp.NextPage
-			issues, resp, err = client.Issues.ListByRepo(user, repo, opt)
-		}
-	}
-}
 
 func main() {
 	args := ArgumentList(os.Args)
@@ -62,5 +16,13 @@ func main() {
 		pieces := strings.Split(githubRepo, "/")
 		githubImport(pieces[0], pieces[1])
 
+	} else if args.GetArgument("--be", "") != "" {
+		beImport()
+	} else {
+		if strings.Count(githubRepo, "/") != 1 {
+			fmt.Fprintf(os.Stderr, "Usage: %s --github user/repo\n", os.Args[0])
+			fmt.Fprintf(os.Stderr, "       %s --be\n", os.Args[0])
+			os.Exit(2)
+		}
 	}
 }
