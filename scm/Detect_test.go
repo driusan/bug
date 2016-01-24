56884aeff38ad9d2293ff9138f3e5554621173cc
Dave MacFarlane <driusan@gmail.com>
2016-01-23 19:32:21 -0500

    Added tests for DetectSCM

---
+++ scm/Detect_test.go
@@ -0,0 +1,104 @@
+package scm
+
+import (
+	"github.com/driusan/bug/bugs"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func TestDetectGit(t *testing.T) {
+	var gdir string
+	gdir, err := ioutil.TempDir("", "gitdetect")
+	if err == nil {
+		os.Chdir(gdir)
+		defer os.RemoveAll(gdir)
+	} else {
+		t.Error("Failed creating temporary directory for detect")
+		return
+	}
+	// Fake a git repo
+	os.Mkdir(".git", 0755)
+
+	options := make(map[string]bool)
+	handler, dir, err := DetectSCM(options)
+	if err != nil {
+		t.Error("Unexpected while detecting repo type: " + err.Error())
+	}
+	if dir != bugs.Directory(gdir+"/.git") {
+		t.Error("Unexpected directory found when trying to detect git repo" + dir)
+	}
+	switch handler.(type) {
+	case GitManager:
+		// GitManager is what we expect, don't fall through
+		// to the error
+	default:
+		t.Error("Unexpected SCMHandler found for Git")
+	}
+
+	// Go somewhere higher in the tree and do it again
+	os.MkdirAll("tmp/abc/hello", 0755)
+	os.Chdir("tmp/abc/hello")
+	handler, dir, err = DetectSCM(options)
+	if err != nil {
+		t.Error("Unexpected while detecting repo type: " + err.Error())
+	}
+	if dir != bugs.Directory(gdir+"/.git") {
+		t.Error("Unexpected directory found when trying to detect git repo" + dir)
+	}
+	switch handler.(type) {
+	case GitManager:
+		// GitManager is what we expect, don't fall through
+		// to the error
+	default:
+		t.Error("Unexpected SCMHandler found for Git")
+	}
+}
+
+func TestDetectHg(t *testing.T) {
+	var gdir string
+	gdir, err := ioutil.TempDir("", "hgdetect")
+	if err == nil {
+		os.Chdir(gdir)
+		defer os.RemoveAll(gdir)
+	} else {
+		t.Error("Failed creating temporary directory for detect")
+		return
+	}
+	// Fake a git repo
+	os.Mkdir(".hg", 0755)
+
+	options := make(map[string]bool)
+	handler, dir, err := DetectSCM(options)
+	if err != nil {
+		t.Error("Unexpected while detecting repo type: " + err.Error())
+	}
+	if dir != bugs.Directory(gdir+"/.hg") {
+		t.Error("Unexpected directory found when trying to detect git repo" + dir)
+	}
+	switch handler.(type) {
+	case HgManager:
+		// HgManager is what we expect, don't fall through
+		// to the error
+	default:
+		t.Error("Unexpected SCMHandler found for Mercurial")
+	}
+
+	// Go somewhere higher in the tree and do it again
+	os.MkdirAll("tmp/abc/hello", 0755)
+	os.Chdir("tmp/abc/hello")
+	handler, dir, err = DetectSCM(options)
+	if err != nil {
+		t.Error("Unexpected while detecting repo type: " + err.Error())
+	}
+	if dir != bugs.Directory(gdir+"/.hg") {
+		t.Error("Unexpected directory found when trying to detect git repo" + dir)
+	}
+	switch handler.(type) {
+	case HgManager:
+		// GitManager is what we expect, don't fall through
+		// to the error
+	default:
+		t.Error("Unexpected SCMHandler found for Mercurial")
+	}
+}
